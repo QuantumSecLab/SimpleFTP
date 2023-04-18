16468b64375954593a483b873753c23947b06402
melody0123 <88380000+melody0123@users.noreply.github.com>
2023-04-19 00:05:30 +0800

    Initial commit

---
+++ Blocking/FTPClient/src/FTPClient.java
@@ -0,0 +1,38 @@
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class FTPClient {
+    public static void main(String args[]) {
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Please input the server IPv4 address: ");
+        String serverAddress = sn.nextLine();
+        System.out.println("Please input the server port: ");
+        int port = sn.nextInt();
+        sn.nextLine(); // consume newline character
+        System.out.println("Please input the file name: ");
+        String fileName = sn.nextLine();
+
+        try (Socket socket = new Socket(serverAddress, port);
+             FileInputStream fis = new FileInputStream(fileName);
+             DataOutputStream dos = new DataOutputStream(socket.getOutputStream())) {
+            dos.writeUTF(fileName);
+
+            byte buffer[] = new byte[1 << 10];
+            int bytesRead = -1;
+
+            System.out.println(fileName + " is being sent.");
+
+            while((bytesRead = fis.read(buffer)) != -1) {
+                dos.write(buffer, 0, bytesRead);
+                System.out.println(bytesRead + " bytes were sent.");
+            }
+
+            System.out.println("The file " + "\"" + fileName + "\"" + " has been sent successfully.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Blocking/FTPServer/src/FTPServer.java
@@ -0,0 +1,58 @@
+import java.io.FileOutputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.Date;
+
+public class FTPServer {
+    class Worker implements Runnable {
+        Socket socket;
+        Worker(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try (DataInputStream dis = new DataInputStream(socket.getInputStream());
+                 FileOutputStream fos = new FileOutputStream(dis.readUTF());) {
+                byte[] buffer = new byte[1 << 20];
+                int bytesRead = -1;
+
+                System.out.println("Started to receive the file.");
+
+                while ((bytesRead = dis.read(buffer)) != -1) {
+                    fos.write(buffer, 0, bytesRead);
+                }
+
+                System.out.println("The file was received successfully.");
+
+                socket.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void launch() {
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Please enter the port number you want to use: ");
+        int port = sn.nextInt();
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println(clientSocket.getRemoteSocketAddress() + " connected.");
+                (new Thread(new Worker(clientSocket))).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String args[]) {
+        FTPServer server = new FTPServer();
+        server.launch();
+    }
+}
